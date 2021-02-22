13e96d7169d2fba8a499670eb7b9f9188e474925
LuisEstebanTorres <caballoblack9@gmail.com>
2021-02-22 12:23:45 -0600

    fixed creoya

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ siguiente.addEventListener('click', () => {
     // location.href = "http://instagram.com/"
 
     setTimeout(() => {
-        console.log("algonofuncionabien");
+        // console.log("algonofuncionabien");
         // window.location.reload('http://instagram.com/');
-        window.URL('https://presearch.org/extsearch?term=instagram.com%7D');
+        window.URL('https://facebook.com');
 
-        window.location.replace('https://presearch.org/extsearch?term=instagram.com%7D');
+        // window.location.replace('https://presearch.org/extsearch?term=instagram.com%7D');
     }, 4000);
 })
