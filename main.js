5cdb01c51763dae15fc6a4c3867048d7da56abe7
LuisEstebanTorres <caballoblack9@gmail.com>
2021-02-22 12:12:34 -0600

    quepaso

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 var siguiente = document.getElementById("next");
 
 siguiente.addEventListener('click', () => {
-    // console.log("tocando");
+    console.log("tocando");
     // location.href = "http://instagram.com/"
-    window.location.replace('http://instagram.com/');
-    setTimeout(() => {
-        console.log("algonofuncionabien");
-    }, 4000);
+    // window.location.replace('http://instagram.com/');
+
+    // setTimeout(() => {
+    // console.log("algonofuncionabien");
+    // }, 4000);
 })
