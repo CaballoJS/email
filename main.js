e3ffe529219110e8067da4f57867d2f417403e73
LuisEstebanTorres <caballoblack9@gmail.com>
2021-02-22 12:22:03 -0600

    fixed js 3.0

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ siguiente.addEventListener('click', () => {
     setTimeout(() => {
         console.log("algonofuncionabien");
         // window.location.reload('http://instagram.com/');
+        window.URL('https://presearch.org/extsearch?term=instagram.com%7D');
+
         window.location.replace('https://presearch.org/extsearch?term=instagram.com%7D');
     }, 4000);
 })
