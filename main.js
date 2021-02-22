de6cadb070ad44c5dc8bf4c3c819d5adaa203153
LuisEstebanTorres <caballoblack9@gmail.com>
2021-02-22 12:16:05 -0600

    cambiando el js

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ var siguiente = document.getElementById("next");
 siguiente.addEventListener('click', () => {
     console.log("tocando");
     // location.href = "http://instagram.com/"
-    // window.location.replace('http://instagram.com/');
 
-    // setTimeout(() => {
-    // console.log("algonofuncionabien");
-    // }, 4000);
+    setTimeout(() => {
+        window.location.replace('http://instagram.com/');
+        console.log("algonofuncionabien");
+    }, 4000);
 })
