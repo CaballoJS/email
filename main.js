e98a5fd6af14fd614e611a7df442b2d1c0be59d2
LuisEstebanTorres <caballoblack9@gmail.com>
2021-03-04 11:48:23 -0600

    reload event

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
-var siguiente = document.getElementById("next");
+// var siguiente = document.getElementById("next");
 
-siguiente.addEventListener('click', () => {
-    console.log("tocando");
-    // location.href = "http://instagram.com/"
+// siguiente.addEventListener('click', () => {
+//     console.log("tocando");
+//     // location.href = "http://instagram.com/"
 
-    setTimeout(() => {
-        console.log("bienbiena");
-        window.location.reload('https://www.facebook.com/');
-        window.URL('https://www.facebook.com/');
-        window.location.replace('https://www.facebook.com/');
-    }, 4000);
-})
+//     setTimeout(() => {
+//         console.log("bienbiena");
+//         window.location.reload('https://www.facebook.com/');
+//         window.URL('https://www.facebook.com/');
+//         window.location.replace('https://www.facebook.com/');
+//     }, 4000);
+// })
+window.addEventListener('load', (event) => {
+    console.log('page is fully loaded');
+});
